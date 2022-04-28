217d7d13af84c066baac800eebdf6082b3db76c5
Jorge Alejandro Zabaleta Mazariegos <l18270308@tuxtla.tecnm.mx>
2022-04-28 10:58:48 -0500

    Routes Desing part one

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -6,73 +6,81 @@ use Illuminate\Http\Request;
 
 class ProductoController extends Controller
 {
-    public function index(){
-        if(!empty(Session::get('categoria_id'))){
-            return view("admin.producto.index",compact('productos'));
-        }
-    }
-    public function create(){
-        return view('admin.producto.create');
-    }
-    public function store(Request $request){
-
-        $producto = new Producto($request->all());
-        
-        if($request->hasFile('urlfoto')){
-
-            $imagen = $request->file('urlfoto');
-            $nuevonombre = Str::slug($request->nombre).'.'.$imagen->guessExtension();
-            $imagen->move(public_path().'/storage/productos/',$nuevonombre)
-            ->resize(1024,null,function($constraint){ $constraint->aspectRatio();  })
-            ->save( public_path('/img/producto/'.$nuevonombre));
-
-            $producto->urlfoto = $nuevonombre;
-        }
-
-        $producto->slug    =   Str::slug($request->nombre);
-        $producto->save();
-        return redirect('/admin/producto');
-
-    }
-    public function show($id){
-        $producto = Producto::find($id);
-        return view('admin.producto.show',compact('producto'));
-    }
-    public function update(Request $request,$id){
-
-        $producto = Producto::findOrFail($id);
-        $foto_anterior     = $producto->urlfoto;
-
-
-        if($request->hasFile('urlfoto')){
-
-            $rutaAnterior = public_path('/img/producto/'.$foto_anterior);
-            if(file_exists($rutaAnterior)){ unlink(realpath($rutaAnterior)); }
-
-            $nuevonombre = Str::slug($request->nombre).'.'.$imagen->guessExtension();
-            Image::make($imagen->getRealPath())
-            ->resize(1024,null,function($constraint){ $constraint->aspectRatio();  })
-            ->save( public_path('/img/producto/'.$nuevonombre));
-
-            $producto->urlfoto = $nuevonombre;
-        }
-        $producto->slug    =   Str::slug($request->nombre);
-        $producto->save();
-        return redirect('/admin/producto');
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
     }
 
-    public function edit($id){
-        $producto = Producto::findOrFail($id);
-        return view('admin.producto.edit',compact('producto'));
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('producto.index');
+    }
+
+    public function create()
+    {
+        return view('producto.create');
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function foto($foto)
+    {
+        //
+    }
+
+    public function foto_store(Request $request, $foto)
+    {
+        //
+    }
+
+    public function foto_destroy($foto)
+    {
+        //
+    }
+
+    public function foto_update(Request $request, $foto)
+    {
+        //
+    }
+
+    public function foto_show($foto)
+    {
+        //
     }
 
     
-    public function destroy($id){
-        $producto = Producto::findOrFail($id);
-        if(file_exists($borrar)){ unlink(realpath($borrar)); }
 
-        $producto->delete();
-
-        return redirect('/admin/producto');
-    }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+//Pagina de inicio
+Route::get('/', 'HomeController@index')->name('home');
+
+//CRUD para productos
+Route::get('productos', 'ProductoController@index')->name('productos.index'); //listado de productos
+Route::get('productos/create', 'ProductoController@create')->name('productos.create'); //formulario de crear producto
+Route::post('productos', 'ProductoController@store')->name('productos.store'); //guardar producto
+Route::get('productos/{producto}', 'ProductoController@show')->name('productos.show'); //mostrar producto
+Route::get('productos/{producto}/edit', 'ProductoController@edit')->name('productos.edit'); //formulario de editar producto
+Route::put('productos/{producto}', 'ProductoController@update')->name('productos.update'); //actualizar producto
+Route::delete('productos/{producto}', 'ProductoController@destroy')->name('productos.destroy'); //formulario de eliminar producto
+Route::get('productos/{producto}/foto', 'ProductoController@foto')->name('productos.foto'); //listado de fotos del producto
+Route::post('productos/{producto}/foto', 'ProductoController@foto_store')->name('productos.foto.store'); //guardar foto del producto
+Route::get('productos/{producto}/foto/{foto}', 'ProductoController@foto_foto')->name('productos.foto.foto'); //formulario de ver foto individual del producto
+Route::get('productos/{producto}/foto/{foto}/destroy', 'ProductoController@foto_destroy')->name('productos.foto.destroy'); //eliminar foto del producto
+Route::get('productos/{producto}/foto/{foto}/edit', 'ProductoController@foto_edit')->name('productos.foto.edit'); //formulario de editar foto del producto
+Route::put('productos/{producto}/foto/{foto}', 'ProductoController@foto_update')->name('productos.foto.update'); //actualizar foto del producto
+
+//CRUD para categorias
+Route::get('categorias', 'CategoriaController@index')->name('categorias.index'); //listado de categorias
+Route::get('categorias/create', 'CategoriaController@create')->name('categorias.create'); //formulario de crear categoria
+Route::post('categorias', 'CategoriaController@store')->name('categorias.store'); //guardar categoria
+Route::get('categorias/{categoria}', 'CategoriaController@show')->name('categorias.show'); //mostrar categoria
+Route::get('categorias/{categoria}/edit', 'CategoriaController@edit')->name('categorias.edit'); //formulario de editar categoria
+Route::put('categorias/{categoria}', 'CategoriaController@update')->name('categorias.update'); //actualizar categoria
+Route::delete('categorias/{categoria}', 'CategoriaController@destroy')->name('categorias.destroy'); //formulario de eliminar categoria
+
