006a18788e2d3a87cb57f43eef4fc257fe6d1a64
Jorge Alejandro Zabaleta Mazariegos <l18270308@tuxtla.tecnm.mx>
2022-04-28 11:28:46 -0500

    Creating CRUD in Controllers and routes

--- routes/web.php
+++ routes/web.php
@@ -44,3 +44,38 @@ Route::get('categorias/{categoria}/edit', 'CategoriaController@edit')->name('cat
 Route::put('categorias/{categoria}', 'CategoriaController@update')->name('categorias.update'); //actualizar categoria
 Route::delete('categorias/{categoria}', 'CategoriaController@destroy')->name('categorias.destroy'); //formulario de eliminar categoria
 
+//CRUD para usuarios
+Route::get('usuarios', 'UserController@index')->name('usuarios.index'); //listado de usuarios
+Route::get('usuarios/create', 'UserController@create')->name('usuarios.create'); //formulario de crear usuario
+Route::post('usuarios', 'UserController@store')->name('usuarios.store'); //guardar usuario
+Route::get('usuarios/{usuario}', 'UserController@show')->name('usuarios.show'); //mostrar usuario
+Route::get('usuarios/{usuario}/edit', 'UserController@edit')->name('usuarios.edit'); //formulario de editar usuario
+Route::put('usuarios/{usuario}', 'UserController@update')->name('usuarios.update'); //actualizar usuario
+Route::delete('usuarios/{usuario}', 'UserController@destroy')->name('usuarios.destroy'); //formulario de eliminar usuario
+
+//CRUD para ventas
+Route::get('ventas', 'VentaController@index')->name('ventas.index'); //listado de ventas
+Route::get('ventas/create', 'VentaController@create')->name('ventas.create'); //formulario de crear venta
+Route::post('ventas', 'VentaController@store')->name('ventas.store'); //guardar venta
+Route::get('ventas/{venta}', 'VentaController@show')->name('ventas.show'); //mostrar venta
+Route::get('ventas/{venta}/edit', 'VentaController@edit')->name('ventas.edit'); //formulario de editar venta
+Route::put('ventas/{venta}', 'VentaController@update')->name('ventas.update'); //actualizar venta
+Route::delete('ventas/{venta}', 'VentaController@destroy')->name('ventas.destroy'); //formulario de eliminar venta
+
+//CRUD para compras
+Route::get('compras', 'CompraController@index')->name('compras.index'); //listado de compras
+Route::get('compras/create', 'CompraController@create')->name('compras.create'); //formulario de crear compra
+Route::post('compras', 'CompraController@store')->name('compras.store'); //guardar compra
+Route::get('compras/{compra}', 'CompraController@show')->name('compras.show'); //mostrar compra
+Route::get('compras/{compra}/edit', 'CompraController@edit')->name('compras.edit'); //formulario de editar compra
+Route::put('compras/{compra}', 'CompraController@update')->name('compras.update'); //actualizar compra
+Route::delete('compras/{compra}', 'CompraController@destroy')->name('compras.destroy'); //formulario de eliminar compra
+
+//CRUD para preguntas
+Route::get('preguntas', 'PreguntaController@index')->name('preguntas.index'); //listado de preguntas
+Route::get('preguntas/create', 'PreguntaController@create')->name('preguntas.create'); //formulario de crear pregunta
+Route::post('preguntas', 'PreguntaController@store')->name('preguntas.store'); //guardar pregunta
+Route::get('preguntas/{pregunta}', 'PreguntaController@show')->name('preguntas.show'); //mostrar pregunta
+Route::get('preguntas/{pregunta}/edit', 'PreguntaController@edit')->name('preguntas.edit'); //formulario de editar pregunta
+Route::put('preguntas/{pregunta}', 'PreguntaController@update')->name('preguntas.update'); //actualizar pregunta
+Route::delete('preguntas/{pregunta}', 'PreguntaController@destroy')->name('preguntas.destroy'); //formulario de eliminar pregunta
